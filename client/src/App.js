1c0be24289a321617b643741d831e94d9e6bb041
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2021-08-01 16:55:40 +0300

    about page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import TopBar from "./components/topBar/topBar"
 import HomePage from "./pages/home/home"
+import About from "./pages/about/about"
 import "./App.css"
 function App() {
   return (
     <div className="App">
       <TopBar></TopBar>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <About></About>
     </div>
   );
 }
