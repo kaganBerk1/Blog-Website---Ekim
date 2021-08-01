82cba70553183c278033e35f25d92b1eb5aeb1cd
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2021-08-01 19:03:13 +0300

    title changed

--- client/src/pages/contact/contact.js
+++ client/src/pages/contact/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <div>
             <div className="title">
                 <span className="firstTitle">
-                    Bize Buralardan 
+                    Bize Bu Bağlantılardan
                 </span>
                 <span className="secondTitle">
                     Ulaşabilirsiniz. 
