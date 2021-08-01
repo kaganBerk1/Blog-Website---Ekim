54dc5ba523a86c37fca611e56039235c5ead7de0
bsrn13 <busrasrn0@gmail.com>
2021-08-01 23:59:59 +0300

    user, post, category

---
+++ api/models/Category.js
@@ -0,0 +1,14 @@
+const { strict } = require("assert/strict");
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
