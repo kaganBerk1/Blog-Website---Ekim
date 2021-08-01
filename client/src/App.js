8f4b6653867b1d88b465531f9bf6e0bf1e3d0528
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2021-08-01 19:00:50 +0300

    all posts page done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/home"
 import About from "./pages/about/about"
 import Single from "./pages/single/single"
 import Contact from "./pages/contact/contact"
+import AllPosts from "./pages/allPosts/allPosts"
 import "./App.css"
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <HomePage></HomePage> */}
       {/* <About></About> */}
       {/* <Single></Single> */}
-      <Contact></Contact>
+      {/* <Contact></Contact> */}
+      <AllPosts></AllPosts>
     </div>
   );
 }
---
+++ client/src/pages/allPosts/allPosts.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Post from '../../components/posts/post/post'
+import "./allPosts.css"
+import Footer from '../../components/footer/footer'
+export default function AllPosts() {
+    return (
+        <>
+        <div className="title">
+                Tüm Yazılarımız 😅
+            </div>
+        <div className="allPosts">
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+            <Post></Post>
+        </div>
+        <Footer></Footer>
+        </>
+    )
+}
