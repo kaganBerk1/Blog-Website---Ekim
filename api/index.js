128363541cc989e4f3f97c16f3db74805904eb7e
bsrn13 <busrasrn0@gmail.com>
2021-08-02 02:16:19 +0300

    post error

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,9 +14,9 @@ mongoose
     useCreateIndex: true,
   })
   .then(console.log("Connected to MongoDB"))
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5500", () => {
   console.log("Backend is running.");
--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,3 @@
-const { strict } = require("assert/strict");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
